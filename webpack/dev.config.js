599ed6d5e68b43a5b54fc0f5df0fd561d0413bc2
Senn <shaunsenn2019@gmail.com>
2021-10-13 17:46:57 -0600

    trying to figure out options issue with sass loader

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -44,7 +44,7 @@ module.exports = webpackMerge(webpackCommon, {
             }
           },
           {
-            loader: 'sass-loader',
+            loader: 'sass-loader'
             //options: {
               // outputStyle: 'expanded',
               // sourceMap: true,
